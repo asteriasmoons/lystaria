1e54da638bf989103986854478e28989ce086360
Asteria Moon <asteriasmoons@outlook.com>
2025-12-17 16:00:07 -0600

    Improved RSS feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -14,6 +14,10 @@ export async function GET(context) {
     description:
       "Where the mystical meets the mundane. A space for exploring magic, personal growth, mental wellness, and the philosophies that shape us.",
     site: context.site,
+    xmlns: {
+      atom: "http://www.w3.org/2005/Atom",
+    },
+    customData: `<language>en-us</language><atom:link href="${context.site}rss.xml" rel="self" type="application/rss+xml" />`,
     items: sortedPosts.map((post) => ({
       title: post.data.title,
       pubDate: post.data.pubDate,
@@ -21,6 +25,5 @@ export async function GET(context) {
       link: `/blog/${post.slug}/`,
       categories: post.data.tags || [],
     })),
-    customData: `<language>en-us</language>`,
   });
 }
