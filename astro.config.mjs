c342bbe8bb03487c6099bd9a5405097065ed067b
Asteria Moon <asteriasmoons@outlook.com>
2025-12-21 16:22:37 -0600

    google console

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   integrations: [mdx()],
