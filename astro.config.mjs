f320568018c46f0e62c3f858132d7e24dd397da8
Asteria Moon <asteriasmoons@outlook.com>
2026-01-07 07:37:03 -0600

    ugh typo

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,19 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel/serverless"; // ← ADD THIS LINE
+import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
   integrations: [mdx(), sitemap()],
   site: "https://lystaria.im",
   output: "server",
   adapter: vercel(),
+
+  vite: {
+    resolve: {
+      alias: {
+        "@": "/src",
+      },
+    },
+  },
 });
